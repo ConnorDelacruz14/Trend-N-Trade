f7d15f5c088cd353cb3779a395033da1835e91a1
ezekielchow <ezekiec@uci.edu>
2024-05-17 10:29:19 -0700

    route updates

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,18 +1,21 @@
 import React, { Suspense, lazy } from 'react';
 import {
+    Route,
     BrowserRouter as Router,
     Routes,
-    Route,
 } from 'react-router-dom';
 
 const Home = lazy(() => import('../pages/Home/Home'));
 const Listing = lazy(() => import('../pages/Listing/Listing'));
 const SignUp = lazy(() => import('../pages/SignUp/SignUp'));
-const Login = lazy(() => import('../pages/Login/Login'))
-const EditProfile = lazy(() => import('../pages/EditProfile/EditProfile'))
-const Search = lazy(() => import('../pages/Search/Search'))
-const Cart = lazy(() => import('../pages/Cart/Cart'));
-const Purchases = lazy(() => import('../pages/Purchases/Purchases'))
+const Login = lazy(() => import('../pages/Login/Login'));
+const About = lazy(() => import('../pages/About/About'));
+const Payment = lazy(() => import('../pages/Payment/Payment'));
+const Contact = lazy(() => import('../pages/Contact/Contact'));
+const Help= lazy(() => import('../pages/Help/Help'));
+const Profile= lazy(() => import('../pages/Profile/profile'));
+
+
 
 const AppRoutes: React.FC = () => {
     return (
@@ -20,13 +23,18 @@ const AppRoutes: React.FC = () => {
             <Suspense fallback={<div>Loading...</div>}>
                 <Routes>
                     <Route path="/" element={<Home />} />
-                    <Route path="/listing/:listingId" element={<Listing />} />
+                    <Route path="/listing" element={<Listing />} />
                     <Route path="/signup" element={<SignUp />} />
-                    <Route path="/edit" element={<EditProfile />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/purchases" element={<Purchases/>} />
                     <Route path="/login" element={<Login/>}></Route>
-                    <Route path="/cart" element={<Cart/>} />
+                    <Route path="/Home" element={<Profile />} />
+                    <Route path="/About" element={<About />} />
+                    <Route path="/Payment" element={<Payment />} />
+                    <Route path="/Contact" element={<Contact />} />
+                    <Route path="/Cart" element={<Home />} />
+                    <Route path="/Help" element={<Help />} />
+                    <Route path="/Profile" element={<Profile />} />
+
+
                 </Routes>
             </Suspense>
         </Router>
