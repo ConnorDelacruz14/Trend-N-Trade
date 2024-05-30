0216e5c05efd8fc41df119d193549bbc152b8ad3
Connor Delacruz <ConnorD1432@gmail.com>
2024-05-29 23:45:11 -0700

    page is done

--- src/pages/EditListing/EditListing.tsx
+++ src/pages/EditListing/EditListing.tsx
@@ -3,48 +3,95 @@ import { BiImageAdd } from "react-icons/bi";
 import './editlisting.css';
 import Header from "../../components/Header/Header.tsx";
 import Footer from "../../components/Footer/Footer.tsx";
+import Listing from "../../types/index.ts"
+
+const defaultListing: Listing = {
+    _id: "",
+    name: "",
+    tags: [],
+    images: ['', '', '', '', '', '', '', ''],
+    description: "",
+    brand: "",
+    listingUserId: "",
+    listingPrice: "",
+    cartStatus: [],
+    offerStatus: [],
+    postingDate: new Date(),
+    condition: "",
+    numLikes: 0
+}
 
 const EditListing: React.FC = () => {
     const colorOptions = ['None', 'Red', 'Blue', 'Green', 'Yellow', 'Black', 'White', 'Purple', 'Orange', 'Pink', 'Brown'];
     const [selectedColor, setSelectedColor] = useState<string>('None');
     const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+    const [listing, setListing] = useState<Listing>(defaultListing);
 
     const handleColorSelect = (color: string) => {
         setSelectedColor(color);
         setDropdownOpen(false);
     };
 
+    const handleImageUpload = (index: number, event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const newUploadedImages = [...listing.images];
+            newUploadedImages[index] = URL.createObjectURL(file);
+            setListing({...listing, images: newUploadedImages});
+        }
+    };
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        console.log(listing);
+    };
+
     return (
         <div className="edit-listing-page-container">
-            <Header/>
+            <Header />
             <div className="edit-listing-page">
-                <div className="edit-listing-container">
+                <form className="edit-listing-container" onSubmit={handleSubmit}>
                     <h2>List an item</h2>
+                    <section>
+                        <h3>Name</h3>
+                        <input type="text" onChange={(e) => setListing({ ...listing, name: e.target.value })} />
+                    </section>
                     <section className="edit-photos">
                         <h3>Photos</h3>
                         <p>Add up to 8 photos in JPEG or PNG format</p>
                         <div className="listing-photos-container">
-                            {[...Array(8)].map((_, index) => (
+                            {listing.images.map((image, index) => (
                                 <div key={index} className="listing-blank-image">
-                                    <label htmlFor={`image-upload-${index}`}>
-                                        <BiImageAdd size={30}/>
-                                        <p>Add a photo</p>
-                                    </label>
-                                    <input type="file" id={`image-upload-${index}`} />
+                                    {image ? (
+                                        <img src={image} alt={`Uploaded ${index}`} className="listing-uploaded-image" />
+                                    ) : (
+                                        index === 0 || listing.images[index - 1] ? (
+                                            <label htmlFor={`image-upload-${index}`}>
+                                                <BiImageAdd size={30} />
+                                                <p>Add a photo</p>
+                                            </label>
+                                        ) : null
+                                    )}
+                                    <input
+                                        type="file"
+                                        id={`image-upload-${index}`}
+                                        style={{ display: 'none' }}
+                                        onChange={(e) => handleImageUpload(index, e)}
+                                    />
                                 </div>
                             ))}
                         </div>
                     </section>
                     <section className="edit-description">
                         <h3>Description</h3>
-                        <textarea cols={90} rows={5}></textarea>
+                        <textarea cols={90} rows={5} onChange={(e) => {setListing({...listing, description: e.target.value})}}></textarea>
                     </section>
                     <section className="edit-dropdowns">
                         <h3>Info</h3>
                         <p>Brand</p>
-                        <input type="text" value="Other"/>
+                        <input type="text" defaultValue="Other" onChange={(e) => {setListing({...listing, brand: e.target.value})}} />
                         <p>Condition</p>
-                        <select id="edit-condition">
+                        <select id="edit-condition" onChange={(e) => {setListing({...listing, condition: e.target.value})}}>
                             <option value="brand-new">Brand new - Unused with original packaging or tags</option>
                             <option value="like-new">Like new - Mint condition pre-owned or new without tags</option>
                             <option value="lightly-used">Used - Lightly used but no noticeable flaws</option>
@@ -55,7 +102,7 @@ const EditListing: React.FC = () => {
                     <section className="edit-tags">
                         <h3>Tags</h3>
                         <div className="edit-tags-container">
-                            <ListingTag name="nike"/>
+                            <ListingTag name="nike" />
                         </div>
                     </section>
                     <section className="edit-enhance">
@@ -63,14 +110,20 @@ const EditListing: React.FC = () => {
                         <p>Colors</p>
                         <div className="custom-dropdown">
                             <div className="custom-dropdown-selected" onClick={() => setDropdownOpen(!dropdownOpen)}>
-                                <div className="color-circle" style={{ backgroundColor: selectedColor.toLowerCase() === 'none' ? 'transparent' : selectedColor.toLowerCase(), border: selectedColor.toLowerCase() === 'none' ? '1px solid black' : 'none' }}></div>
+                                <div className="color-circle" style={{
+                                    backgroundColor: selectedColor.toLowerCase() === 'none' ? 'transparent' : selectedColor.toLowerCase(),
+                                    border: selectedColor.toLowerCase() === 'none' ? '1px solid black' : 'none'
+                                }}></div>
                                 <span>{selectedColor}</span>
                             </div>
                             {dropdownOpen && (
                                 <div className="custom-dropdown-list">
                                     {colorOptions.map(color => (
                                         <div key={color} className="custom-dropdown-option" onClick={() => handleColorSelect(color)}>
-                                            <div className="color-circle" style={{ backgroundColor: color.toLowerCase() === 'none' ? 'transparent' : color.toLowerCase(), border: color.toLowerCase() === 'none' ? '1px solid black' : 'none' }}></div>
+                                            <div className="color-circle" style={{
+                                                backgroundColor: color.toLowerCase() === 'none' ? 'transparent' : color.toLowerCase(),
+                                                border: color.toLowerCase() === 'none' ? '1px solid black' : 'none'
+                                            }}></div>
                                             <span>{color}</span>
                                         </div>
                                     ))}
@@ -78,18 +131,19 @@ const EditListing: React.FC = () => {
                             )}
                         </div>
                         <p>Location</p>
-                        <input type="text"/>
+                        <input type="text" />
                     </section>
                     <section className="edit-shipping">
                         <h3>Shipping</h3>
                     </section>
                     <section className="edit-price">
                         <h3>Price</h3>
-                        <input type="text"/>
+                        <input type="text" onChange={(e) => setListing({...listing, listingPrice: e.target.value})} />
                     </section>
-                </div>
+                    <button className="listing-submit-button" type="submit">Create Listing</button>
+                </form>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
 };
